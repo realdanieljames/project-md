cc49d75d6458d83448578eab7d2f90e578fcf125
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-03-08 18:39:25 -0500

    added show/no-show button for edit-control component. Next i will make the dropdown selection box within the edit controls component

--- src/App.js
+++ src/App.js
@@ -4,33 +4,35 @@ import EditControlComponent from "./components/DComponents/EditControlComponent/
 
 class App extends Component {
   state = {
-    item: [
+    items: [
       {
-        Name: "1",
-        Price: "1",
-        ImageLink: "1",
-        Description: "This Item represents the first (1) item on the list",
+        name: "1",
+        price: "1",
+        imageLink: "1.jpeg",
+        description: "This Item represents the first (1) item on the list",
       },
       {
-        Name: "",
-        Price: "",
-        ImageLink: "",
-        Description: "This Item represents the second (2) item on the list",
+        name: "2",
+        price: "2",
+        imageLink: "2.jpeg",
+        description: "This Item represents the second (2) item on the list",
       },
       {
-        Name: "",
-        Price: "",
-        ImageLink: "",
-        Description: "This Item represents the third (3) item on the list",
+        name: "3",
+        price: "3",
+        imageLink: "3.jpeg",
+        description: "This Item represents the third (3) item on the list",
       },
-    ],
+    ]
+
   };
+
 
   render() {
     return (
       <div className="App">
         <h1>Grocery Store Catalogue</h1>
-        <EditControlComponent />
+        <EditControlComponent items={this.state.items} showEditControls={this.state.showEditControls}/>
       </div>
     );
   }
--- src/components/DComponents/EditControlComponent/EditControlComponent.js
+++ src/components/DComponents/EditControlComponent/EditControlComponent.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./EditControlComponent.css";
 
-const EditControlComponent = () => {
+//========================================================================================//
+//========================================================================================//
+
+const EditControlComponent = (props) => {
 
 
-    const openEditControlComponent = ()=>{
-        console.log('button')
+const [showEditControls, setShowEditControls] = useState(false);
 
+//========================================================================================//
+//========================================================================================//
+
+const openEditControlComponent = () => {
+    if (showEditControls === true) {
+    setShowEditControls(false);
+    } else {
+    setShowEditControls(true);
     }
-    return (
-        <div>
-            <button onClick={openEditControlComponent}>Edit Item</button>
+};
+//========================================================================================//
+//========================================================================================//
+return (
+    <div>
+    <button onClick={openEditControlComponent}>Edit Item</button>
 
+    {showEditControls ? (
+        <div className="edit-item-container">
+        <h3 className="edit-control-header">Edit Item</h3>
+
+        <input className="name-input" placeholder="Name" />
+        <input className="price-input" placeholder="Price" />
+        <input className="image-input" placeholder="Image" />
+        <input className="description-input" placeholder="Description" />
+        <button className="submit-button">Submit</button>
         </div>
-    )
-}
+    ) : null}
+    </div>
+);
+};
 
-export default EditControlComponent
+//========================================================================================//
+//========================================================================================//
+
+export default EditControlComponent;
